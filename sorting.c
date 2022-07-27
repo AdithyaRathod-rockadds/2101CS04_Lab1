14acd39bed4a5d2959e8ba7693e70b3911f96c2e
unknown <adithya_2101cs04@iitp.ac.in>
2022-07-27 14:58:58 +0530

    bubble sort commit

--- sorting.c
+++ sorting.c
@@ -1,7 +1,47 @@
 #include<stdio.h>
+
+void bubble_sort(int arr[], int n)
+{
+    int k;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=i+1; j<n; j++)
+        {
+            if (arr[j]<arr[i])
+            {
+                k=arr[i];
+                arr[i]=arr[j];
+                arr[j]=k;
+            }
+        }
+    }
+
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+}
 int main()
 {   
     //initialization
-   int arr[]={2,1,5,3,4,8,7,6,9};
-   int n=sizeof(arr)/sizeof(arr[0]);
+   int arr[]={2,1,5,3,4,8,7,9,6}; //array initialization
+   int n=sizeof(arr)/sizeof(arr[0]); //size of array
+    
+
+    //before 
+    printf("The array before sorting is:\n");
+    print_array(arr, n);
+    printf("\n");
+
+    bubble_sort(arr, n);//sorting method
+
+    //after
+    printf("The array after sorting is:\n");
+    print_array(arr, n);
+
+
 }
